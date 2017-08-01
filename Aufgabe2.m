dbb66293b3b3209a414518718a5110b6c5c4e5ba
Don-Angelo <Marcel.Grandinetti@gmail.com>
2017-08-01 18:24:45 +0200

    Audiosignal einlesen

--- Aufgabe2.m
+++ Aufgabe2.m
@@ -7,3 +7,31 @@
 %}
 clear
 
+%% Einlesen
+
+abtastrate=44100;                               %Hz
+%aufloesung=16;                                 %Bit
+[tonspur,abtastrate]=audioread('Aufgabe5.wav');
+signallaenge=size(tonspur,1)/abtastrate;        %30sec
+
+tSigPlot=0:1/abtastrate:signallaenge-1/abtastrate;
+figure(1);
+plot(tSigPlot,tonspur);
+
+%% FFT
+fMax=20000;
+spektrum=fftshift(fft(tonspur,abtastrate)/abtastrate);
+
+fSchritte=fMax/size(spektrum,1);
+fAchse=0:fSchritte:fMax-fSchritte;
+figure(2);
+plot(fAchse,abs(spektrum));
+
+
+%% Testsection
+
+
+
+%% Tonspur abspielen
+
+sound(tonspur,abtastrate);
