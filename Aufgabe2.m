29a31f76946e08948f94297c9b6a364ffc8b5377
Don-Angelo <Marcel.Grandinetti@gmail.com>
2017-08-02 18:12:21 +0200

    Aufgabe 2 funktionsfähig

--- Aufgabe2.m
+++ Aufgabe2.m
@@ -4,13 +4,15 @@
     Autor: Marcel Grandinetti
     Datum:01.08.2017
     Beschribung:    
+                    For-Schleife da vector mit den orten der Peaks aber
+                    keine möglichkeit bereiche dahinzu zu fügen
 %}
 clear
 
 %% Einlesen
 
 [tonspur,abtastrate]=audioread('Aufgabe2.wav');
-signallaenge=size(tonspur,1)/abtastrate;        %30sec
+signallaenge=size(tonspur,1)/abtastrate;        
 
 tSigPlot=0:1/abtastrate:signallaenge-1/abtastrate;
 figure(1);
@@ -30,38 +32,61 @@ plot(fAchse,abs(spektrum(1:fftRate/2+1))); % Plot Amplitudenspektrum
 
 %% Filtern und Rücktransformieren
 
+%Spektrum in Imaginär- und Realteil aufteilen
+spektrumReal=real(spektrum);
+spektrumImag=imag(spektrum);
 
+%Frequenzachse
+f=linspace(0,size(spektrum,1),size(spektrum,1));
 
-% bandbreite=100;
-% [M,I]=max(abs(spektrum(:,1)))
-% st=I-bandbreite:1:I+bandbreite;
-% spektrum(st,:)=0;
+%Plot reales Spektrum
+subplot(2,2,1);
+plot(f,spektrumReal); % Plot Amplitudenspektrum
+findpeaks(abs(spektrumReal(:,1)),f,'Threshold',1e-3);
+title('Realteil ungefiltert');
 
-% anfang=mean(abs(spektrum(:,1)))    
-% while (mean(abs(spektrum(:,1))) > 0.000005)
-%     bandbreite=200;
-%     [M,I]=max(abs(spektrum));
-%     st=I-bandbreite:1:I+bandbreite;
-%     spektrum(st,:)=0;   
-% end
-% ende=mean(abs(spektrum(:,1)))
+%Plot imaginäres Spektrum
+subplot(2,2,2);
+plot(f,spektrumImag);
+findpeaks(abs(spektrumImag(:,1)),f,'Threshold',1e-3);
+title('Imaginärteil ungefiltert');
 
+%Filtern reales Spektrum
+[pks,locs,w,p] = findpeaks(abs(spektrumReal(:,1)),f,'Threshold',1e-3);
+locs=int32(locs);
+ for x = 1:size(locs,2)
+     bandbreite=int32(w(x)*150);
+     st=locs(x)-bandbreite:1:locs(x)+bandbreite;
+     spektrumReal(st,:)=0;
+ end
 
-figure(3);
-plot(fAchse,abs(spektrum(1:fftRate/2+1))); % Plot Amplitudenspektrum
+%Filtern imaginäres Spektrum
+[pks,locs,w,p] = findpeaks(abs(spektrumImag(:,1)),f,'Threshold',1e-3);
+locs=int32(locs);
+ for x = 1:size(locs,2)
+     bandbreite=int32(w(x)*150);
+     st=locs(x)-bandbreite:1:locs(x)+bandbreite;
+     spektrumImag(st,:)=0;
+ end
 
+%Plot reales Spektrum nach dem Filtern
+subplot(2,2,3);
+plot(f,spektrumReal); 
+title('Realteil gefiltert');
 
+%Plot imaginäres Spektrum nach dem Filtern
+subplot(2,2,4);
+plot(f,spektrumImag);
+title('Imaginärteil gefiltert');
+
+%Komplexes Spektrum wieder zusammensetzen
+spektrumZsm=complex(spektrumReal,spektrumImag);
 
 %IFFT
-tonNeu=ifft(spektrum,fftRate)*fftRate;
-
-%% Testsection
-
-
-
+tonNeu=real(ifft(spektrumZsm,fftRate)*fftRate);
 
 
 %% Tonspur abspielen
 
 sound(tonNeu,abtastrate);
-
+audiowrite('EntstoertA2.mp3',tonNeu,abtastrate);
