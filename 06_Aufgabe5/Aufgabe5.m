6b95685cd47e697d297fd2f2bbe671c50d4f68e6
Don-Angelo <Marcel.Grandinetti@gmail.com>
2017-08-30 22:07:51 +0200

    A5 bearbeitet

--- 06_Aufgabe5/Aufgabe5.m
+++ 06_Aufgabe5/Aufgabe5.m
@@ -1,22 +1,25 @@
 % Dimensionierung einer kontinuierlichen PLL zur FM Demodulation
+
 clear
+
 % RF Parameter
-f=1e6; % Trägerfrequenz
+f=9000; % Trägerfrequenz
 df=25e3; % Frequenzhub bei FM Modulation
 fmod=10e3; % Modulationsfrequenz
+
 % Loop Parameter
 Kd=0.5; % Definition des Phasendetektorgewinns
-K0=2*pi*1e5; % Definition der VCO Konstante in rad/Hz
-w0=2*pi*50e3; % Definition der PLL Eigenfrequenz
+K0=2*pi*100000; % Definition der VCO Konstante in rad/Hz
+w0=2*pi*50000; % Definition der PLL Eigenfrequenz
 zeta=0.7; % Definition der Dämpfung der PLL Regelschleife
 % Analoges Loop Regelfilter habe die Form: F(s) = (1 + tau2*s)/(1 + tau1*s)
 tau1=K0*Kd/w0^2;
 tau2=2*zeta/w0-1/(K0*Kd);
 % Simulink Simulation der zeitkontinuierlichen PLL (Aquisition)
-stop=2.5e-3; % Simulationsende in Sekunden
+stop=1; % Simulationsende in Sekunden
 %Open Simulink Model
-open_system mod_track.mdl
+open_system PLL.slx
 % Parameters for Simulation
-set_param('mod_track', 'StopTime', 'stop')
+set_param('PLL', 'StopTime', 'stop')
 %start Simulation
-sim Aufgabe5
+sim PLL.slx
